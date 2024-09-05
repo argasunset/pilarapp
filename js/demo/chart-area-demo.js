8da2d31ba260562af5ad90d94b6f68fba1b39d27
argsunset <argaramiro09@gmail.com>
2024-09-05 13:22:31 +0700

    eren niga

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -1,4 +1,4 @@
-<script>
+
 // Set new default font family and font color to mimic Bootstrap's default styling
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
@@ -116,4 +116,3 @@ var myLineChart = new Chart(ctx, {
     }
   }
 });
-</script>
